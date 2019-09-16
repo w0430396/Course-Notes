9f9ad3438db6840baec21df6b41bd61848795d36
Mitch Tubrett <w0430396@campus.nscc.ca>
2019-09-16 10:08:17 -0300

    mytriforce

---
+++ intro-to-python/myturtle.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle.color("red")
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+
+turtle.color("blue")
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(200)
+turtle.left(120)
+turtle.forward(100)
+turtle.left(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
